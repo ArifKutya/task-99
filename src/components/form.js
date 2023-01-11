9c00a305930ca00ddac666802a792765f86f4495
Arif <arifkutq63@gmail.com>
2023-01-11 19:36:23 +0200

    work

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,16 @@
 
-import {styles} from "./styles.css";
+import  "./styles.css";
 import React from "react";
 function LoginForm() {
-  return (
-    <div className={styles.card} >
-      <form className={styles.form}>
-        <input className={styles.input} placeholder={"Email"}/>
-        <input className={styles.input} placeholder={"Password"}/>
-        <button className={styles.button}>Login</button>
-      </form>
-    </div>
-  )
-}
+
+    return (
+      <div className="card">
+        <form className="form">
+          <input className="input" placeholder={"Email"} />
+          <input className="input" placeholder={"Password"} />
+          <button className="button">Login</button>
+        </form>
+      </div>
+    )
+  }
 export default LoginForm
